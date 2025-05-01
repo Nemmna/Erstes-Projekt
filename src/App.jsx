bcd8d72a425a82c1a35512583a6c6a2e98859896
Nemmna <134049932+Nemmna@users.noreply.github.com>
2025-05-01 05:14:10 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
 import './App.css';
+import SplitText from "./SplitText";
+import AnimatedContent from './module/AnimatedContent';
+import CircularGallery from './CircularGallery';
+
+const handleAnimationComplete = () => {
+  console.log('All letters have animated!');
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>Wunderschönen guten Morgen</h1>
+
+      <h2>Mein erstes Projekt.
+      </h2>
+      <SplitText
+        text="Ich liebe React"
+        className="text-2xl font-semibold text-center"
+        delay={150}
+        animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+        animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+        easing="easeOutCubic"
+        threshold={0.2}
+        rootMargin="-50px"
+        onLetterAnimationComplete={handleAnimationComplete}
+      />
+      <AnimatedContent
+        distance={150}
+        direction="horizontal"
+        reverse={false}
+        config={{ tension: 80, friction: 20 }}
+        initialOpacity={0.2}
+        animateOpacity
+        scale={1.1}
+        threshold={0.2}
+      >
+        <div>Animation</div>
+      </AnimatedContent>
+      <div style={{ height: '600px', position: 'relative' }}>
+        <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
+      </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
     </div>
   );
 }
